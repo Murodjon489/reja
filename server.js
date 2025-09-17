94307ae3eae976afb97fbfa3f30095c8825b05b1
Murodjon489 <murodjonaxmadaliyev599@gmail.com>
2025-09-17 20:45:38 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+//1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//2: Session code
+
+//3: Views
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4: Routing code
+app.get("/hello", function (req, res) {
+  res.end(`HELLO WORLD`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The Server is running sucessfully on port: ${PORT} `);
+});
