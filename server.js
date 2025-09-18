c04adb81ef1367b02fa09cb828727c3b6d633552
Murodjon489 <murodjonaxmadaliyev599@gmail.com>
2025-09-18 19:44:26 +0900

    Loyihani tugatdim

--- server.js
+++ server.js
@@ -14,8 +14,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4: Routing code
-app.get("/hello", function (req, res) {
-  res.end(`HELLO WORLD`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "succes" });
+});
+
+app.get("/", function (req, res) {
+  res.render(`harid`);
 });
 
 const server = http.createServer(app);
