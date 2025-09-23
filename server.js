fec8addc702ee68c372502583983f421b5ee87d2
Murodjon489 <murodjonaxmadaliyev599@gmail.com>
2025-09-23 18:00:49 +0900

    feat: train callback hamda async functionlar, and homework

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
     user = JSON.parse(data);
   }
 });
-
 //1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -29,15 +28,15 @@ app.post("/create-item", (req, res) => {
 });
 
 app.get("/author", (req, res) => {
-  res.render("author", { user: user });
+  res.render("author", { user: user || {} });
 });
 
 app.get("/", function (req, res) {
-  res.render(`harid`);
+  res.render(harid);
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The Server is running sucessfully on port: ${PORT} `);
+  console.log("The Server is running sucessfully on port: ${PORT}");
 });
